4dbf41251ce02e59397b9c1aa6670ca79b2e6657
Celest <150266326+kaps-c@users.noreply.github.com>
2026-06-07 15:26:47 +0800

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+          seen = {}
+          solution = []
+
+          for index, number in enumerate(nums):
+              difference = target - number
+              if difference in seen:
+                  idx1 = seen[difference]
+                  idx2 = index
+                  solution = [idx1, idx2]
+                  return solution
+              else: 
+                  seen[number] = index
+          return solution
