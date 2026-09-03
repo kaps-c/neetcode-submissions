6f925e98b704c182c436e455da19cea5150c5e0b
Celest <150266326+kaps-c@users.noreply.github.com>
2026-09-03 18:38:16 +0800

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        # base case
+        if not root:
+            return 0
+        
+        # recursive case
+        else: 
+            depth_left = 1 + self.maxDepth(root.left)
+            depth_right = 1 + self.maxDepth(root.right)
+            return max(depth_left, depth_right)
+
+        
+        
